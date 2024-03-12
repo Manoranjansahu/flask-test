1a04dfc929fb0187ad8651e89dd275fe4b45328c
Manoranjansahu <96382266+Manoranjansahu@users.noreply.github.com>
2024-03-12 19:01:14 +0530

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Welcome"
+    return render_template("subscriber.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
