dcab5988b73b11b28dbd9f2992c2eb12eb546f31
Manoranjansahu <96382266+Manoranjansahu@users.noreply.github.com>
2024-03-12 14:14:19 +0530

    main.py

--- main.py
+++ main.py
@@ -1,36 +1,32 @@
 import paho.mqtt.client as mqtt
-import sys
-import json
+from flask import Flask, jsonify
+import os
+from threading import Thread
 
+app = Flask(__name__)
+mqtt_broker = "mqtt.eclipseprojects.io"
 mqtt_topic = "TEMPERATURE"
-mqtt_data = {"username": "admin",
-          "password": "admin",
-          "id": "node2",
-          "value": "500"}
+message = ""
 
-def on_connect(client, userdata, flags, rc, a):
-    print(f"{mqtt_data['id']} Connected : {rc}")
-    client.subscribe(mqtt_topic)
-    data = json.dumps(mqtt_data)
-    client.publish(mqtt_topic, data)
+@app.route('/')
+def index():
+    global message
+    return f"{message}"
 
 def on_message(client, userdata, msg):
-    data = json.loads(msg.payload.decode('utf-8', 'ignore'))
-    if data["id"] != mqtt_data["id"]:
-        print(f"Received message on topic {msg.topic}: {data['value']}")
+    global message
+    message = msg.payload.decode('utf-8')
 
-client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-client.on_connect = on_connect
-client.on_message = on_message
+def thread():
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    client.on_message = on_message
+    client.connect(mqtt_broker)
 
-client.connect("mqtt.eclipseprojects.io")
+    client.subscribe("TEMPERATURE")
+    client.loop_forever()
 
-client.loop_start()
 
-while True:
-    num = input("Enter data : ")
-    mqtt_data["value"] = num
-    data = json.dumps(mqtt_data)
-    client.publish(mqtt_topic, data)
-    if num == "0":
-        sys.exit(1)
+if __name__ == '__main__':
+    t1 = Thread(target = thread)
+    t1.start()
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
