993fb6d3984a89ffa1ca8af2ec8c00d7892251d5
Manoranjansahu <96382266+Manoranjansahu@users.noreply.github.com>
2024-03-12 11:40:36 +0530

    main.py

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ from flask import Flask, jsonify
 import os
 
 app = Flask(__name__)
+val = 0
 
 
 @app.route('/')
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask application"})
+
+@app.route('/value')
+def value():
+    global  val
+    val = val + 1
+    return val
 
 
 if __name__ == '__main__':
