4635ef915b86601528d47af7cd4ee707daf554f4
Manoranjansahu <96382266+Manoranjansahu@users.noreply.github.com>
2024-03-12 13:13:29 +0530

    main.py

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
-from flask import Flask, jsonify
-import os
+import paho.mqtt.client as mqtt
+import sys
+import json
 
-app = Flask(__name__)
-val = 0
+mqtt_topic = "TEMPERATURE"
+mqtt_data = {"username": "admin",
+          "password": "admin",
+          "id": "node2",
+          "value": "500"}
 
+def on_connect(client, userdata, flags, rc, a):
+    print(f"{mqtt_data['id']} Connected : {rc}")
+    client.subscribe(mqtt_topic)
+    data = json.dumps(mqtt_data)
+    client.publish(mqtt_topic, data)
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask application"})
+def on_message(client, userdata, msg):
+    data = json.loads(msg.payload.decode('utf-8', 'ignore'))
+    if data["id"] != mqtt_data["id"]:
+        print(f"Received message on topic {msg.topic}: {data['value']}")
 
-@app.route('/value')
-def value():
-    global val
-    val = val + 1
-    return f"{val}"
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+client.on_connect = on_connect
+client.on_message = on_message
 
+client.connect("mqtt.eclipseprojects.io")
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+client.loop_start()
+
+while True:
+    num = input("Enter data : ")
+    mqtt_data["value"] = num
+    data = json.dumps(mqtt_data)
+    client.publish(mqtt_topic, data)
+    if num == "0":
+        sys.exit(1)
