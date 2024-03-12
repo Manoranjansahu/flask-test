0bc06b6be02f456d78a56833dccef01d9d4d6dd9
Manoranjansahu <96382266+Manoranjansahu@users.noreply.github.com>
2024-03-12 14:47:32 +0530

    main.py

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-import socket
+import paho.mqtt.client as mqtt
+from flask import Flask, jsonify
+import os
+from threading import Thread
 
-obj = socket.socket(family = socket.AF_INET, type = socket.SOCK_STREAM)
-obj.bind(("",3456))	
-obj.listen(4)
-clientobj,address = obj.accept()
+app = Flask(__name__)
+mqtt_broker = "mqtt.eclipseprojects.io"
+mqtt_topic = "TEMPERATURE"
+message = ""
 
-conn = True
-while conn:
-    gotmsg = clientobj.recv(1024)
-    gotmsg.decode('utf-8')
-    if len(gotmsg) == 0:
-        conn = False
-        obj.close()
+@app.route('/')
+def index():
+    global message
+    return f"{message}"
+
+def on_message(client, userdata, msg):
+    global message
+    message = msg.payload.decode('utf-8')
+
+def thread():
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    client.on_message = on_message
+    client.connect(mqtt_broker)
+
+    client.subscribe("TEMPERATURE")
+    client.loop_forever()
+
+
+if __name__ == '__main__':
+    t1 = Thread(target = thread)
+    t1.start()
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
