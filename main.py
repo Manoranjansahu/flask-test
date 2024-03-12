788b9381b9f6f295443e58a23ba09609518e414a
Manoranjansahu <96382266+Manoranjansahu@users.noreply.github.com>
2024-03-12 18:41:46 +0530

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify
+import os
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
+@app.route('/')
+def index():
     return "Welcome"
 
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
